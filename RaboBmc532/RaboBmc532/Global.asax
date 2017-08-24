c7862eea988358828b29d6965de336e9eb572d91
Barry Carnahan <carnahan_bmc@hotmail.com>
2017-08-24 23:29:12 +0200

    First project in github repository RaboBmcAzure.

---
+++ RaboBmc532/RaboBmc532/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RaboBmc532.MvcApplication" Language="C#" %>
---
+++ RaboBmc532/RaboBmc532/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RaboBmc532.Startup))]
+namespace RaboBmc532
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
